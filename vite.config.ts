4257b257b1fa65fd302fab9082ccdf0d54e0fb92
eileenarehman-work <eileen.a.rehman@gmail.com>
2026-09-10 08:28:40 -0400

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -66,6 +66,7 @@ function aistudioMediaPlugin(): Plugin {
 
 export default defineConfig(() => {
   return {
+    base: '/ismaili-Center-Houston-Phonebot-websites-app-/
     plugins: [react(), tailwindcss(), aistudioMediaPlugin()],
     resolve: {
       alias: {
